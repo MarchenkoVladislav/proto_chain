2d41a8f8334a7cda82f5383ccd051cddcaa3b966
MarchenkoVladislav <vlad25091999@mail.ru>
2022-06-26 21:18:56 +0300

    Port was changed

--- app.js
+++ app.js
@@ -41,4 +41,6 @@ server.on('request', (req, res) => {
     }
 });
 
-server.listen(3000, () => console.log('Server was started'))
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => console.log('Server has been started'))
